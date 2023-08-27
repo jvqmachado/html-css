e071670bcdd1f83c070a46d4493c6fd68ccb195e
João Vitor Quaquarelli Machado <jobr2108@gmail.com>
2023-08-27 13:03:13 -0300

    criei To Do list

---
+++ projetos/todo-list/script.js
@@ -0,0 +1,55 @@
+const button = document.querySelector('.button-task');
+const input = document.querySelector('.input-task');
+const listaCompleta = document.querySelector('.list-task')
+let minhaListaDeItens = []
+
+function adicionarNovaTarefa() {
+    minhaListaDeItens.push({
+        tarefa: input.value,
+        concluida: false
+    })
+    input.value = ''
+    mostrarTarefas()
+}
+
+function mostrarTarefas() {
+    let novaLi = ''
+
+    minhaListaDeItens.forEach((item, posicao) => {
+        novaLi = novaLi + `
+        <li class="task ${item.concluida && "done"}">
+            <img src="imagens/checked.png" alt="check na tarefa" onclick="concluirTarefa(${posicao})">
+            <p>${item.tarefa}</p>
+            <img src="imagens/trash.png" alt="Tarefa para o lixo" onclick="deletarItem(${posicao})">
+    </li>`
+    })
+
+    listaCompleta.innerHTML = novaLi
+
+    localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+}
+
+function concluirTarefa(posicao) {
+    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+
+    mostrarTarefas()
+}
+
+function deletarItem(posicao) {
+    minhaListaDeItens.splice(posicao, 1)
+    mostrarTarefas()
+}
+
+function recarregarTarefas() {
+    const tarefasDoLocalStorage = localStorage.getItem('lista')
+    
+    if (tarefasDoLocalStorage){
+    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage)
+    }
+   
+    mostrarTarefas() 
+}
+
+recarregarTarefas()
+
+button.addEventListener('click', adicionarNovaTarefa);
